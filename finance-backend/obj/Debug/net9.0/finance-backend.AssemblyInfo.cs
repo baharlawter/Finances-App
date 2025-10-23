5ac5ada6fec5816b0ded373bd644e29a3fc6a100
bahar.lawter@gmail.com <baharlawter@gmail.com>
2025-10-23 11:13:17 -0500

    created tasks.json and wrote the commands

--- finance-backend/obj/Debug/net9.0/finance-backend.AssemblyInfo.cs
+++ finance-backend/obj/Debug/net9.0/finance-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("finance-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7749b5f41844cb20e296c93b301419aef7c9b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("finance-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("finance-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
