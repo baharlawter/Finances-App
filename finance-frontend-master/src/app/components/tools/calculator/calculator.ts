0087ab7f66640c4c2982cacf2063b49ebfb131b7
bahar.lawter@gmail.com <baharlawter@gmail.com>
2025-10-28 13:37:45 -0500

    added simple debt calculator

--- finance-frontend-master/src/app/components/tools/calculator/calculator.ts
+++ finance-frontend-master/src/app/components/tools/calculator/calculator.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculator.html',
-  styleUrl: './calculator.scss'
+  styleUrl: './calculator.scss',
 })
 export class Calculator {
+  principal: number = 0;
+  rate: number = 0;
+  time: number = 0;
+  result: number = 0;
 
+  calculate(): void {
+    const rateDecimal = this.rate / 100;
+    this.result = this.totalDebt(this.principal, rateDecimal, this.time);
+  }
+
+  totalDebt(principal: number, rate: number, time: number): number {
+    return principal + principal * rate * time;
+  }
 }
